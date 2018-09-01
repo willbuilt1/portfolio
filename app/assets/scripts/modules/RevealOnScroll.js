a47da360a25bb82e3ca9811d9221ba4ef38a99d9
Will <wfoster12@gmail.com>
2018-09-01 21:50:20 +0100

    amended about layout

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -29,22 +29,6 @@ class RevealOnScroll {
         })
 
     }
-    //Previous method shown below
-    /*
-    createWaypoints(){
-        var that = this;
-        this.itemsToReveal.each(function() {
-            let currentItem =this;
-            new Waypoint({
-                element: currentItem,
-                handler: function(){
-                    $(currentItem).addClass('reveal-item--visible');
-                },
-                offset: that.offsetPercentage
-            })
-        })
-    }
-    */
 }
 
 export default RevealOnScroll;
