fef78c52c0cfc101e919e2b411be62a389d81b2b
Will <wfoster12@gmail.com>
2018-09-17 12:40:45 +0100

    small cahnges made to the animation timings and HTML structure

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,6 +3,6 @@ import $ from 'jquery';
 import RevealOnScroll from './modules/RevealOnScroll';
 
 new RevealOnScroll ('.about', '80%', 1600);
-new RevealOnScroll ('.portfolio', '80%');
+new RevealOnScroll ('.portfolio', '80%', 700);
 new RevealOnScroll ('.spotlight', '80%');
 
